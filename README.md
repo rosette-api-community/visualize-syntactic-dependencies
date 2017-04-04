3e8da3bc74b9d5e06aceadb6018d15ce440e5645
Zachary Yocum <zyocum@basistech.com>
2017-04-04 17:24:29 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# visualize-syntactic-dependencies
-Visualize Rosette API syntactic dependency parse results via GraphViz.
+#Visualizing Dependency Parse Trees
+
+##Introduction
+This repository includes a Python script that can produce graphical renderings of the dependency parse trees as returned by the [Rosette API `syntax/dependencies` endpoint](https://developer.rosette.com/features-and-functions#introduction17).  The script relies on [GraphViz](http://graphviz.org/) to produce SVG image files that can be viewed in a web browser.
+
+##Installation
+###Python Dependencies
+The Python script `deps_to_graph.py` is written for Python 3 (specifically Python 3.6.0, but should be compatible with most versions of Python 3).  To install the Python dependencies it's recommended to setup a virtual environment with `virtualenv`.  To begin, make sure [`virtualenv`](https://virtualenv.pypa.io/en/stable/installation/) is installed and up to date with [`pip`](https://pip.pypa.io/en/stable/installing/):
+
+    $ pip3 install -U virtualenv
+
+Create a virtual environment in the current directory:
+
+    $ python3 $(which virtualenv) .
+
+Then, activate the virtual environment:
+
+    $ source bin/activate
+
+Finally, install the Python 3 dependences from the `requirements.txt` file:
+
+    (parse_tree_viz) $ pip3 install -r requirements.txt
+
+###GraphViz
+`deps_to_graph.py` relies on the `dot` program from the [GraphViz](http://graphviz.org/) package to produce SVG images, so the last step is to install GraphViz.  GraphViz can be installed from most popular managers ([Homebrew](https://brew.sh/), [RPM](http://rpm.org/), etc.).  Once you've installed the GraphViz package, you can check if the `dot` binary is available:
+
+    $ which dot
+
+If the above command prints a path to the `dot` binary, and you've followed the earlier steps to install the Python dependencies, then you're all set to start visualizing some dependency parse trees!
+
+##Usage
+This section describes how to use `deps_to_graph.py`:
+
+    $ ./deps_to_graph.py -h
+    usage: deps_to_graph.py [-h] [-i INPUT] [-u] [-o OUTPUT] [-k KEY] [-a API_URL]
+                            [-l LANGUAGE] [-b]
+    
+    Render Rosette API dependency parse trees as SVG via Graphviz
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -i INPUT, --input INPUT
+                            Path to a file containing input data (if not specified
+                            data is read from stdin) (default: None)
+      -u, --content-uri     Specify that the input is a URI (otherwise load text
+                            from file) (default: False)
+      -o OUTPUT, --output OUTPUT
+                            Path to a file where SVG will be written (if not
+                            specified data is written to stdout) (default: None)
+      -k KEY, --key KEY     Rosette API Key (default: None)
+      -a API_URL, --api-url API_URL
+                            Alternative Rosette API URL (default:
+                            https://api.rosette.com/rest/v1/)
+      -l LANGUAGE, --language LANGUAGE
+                            A three-letter (ISO 639-2 T) code that will override
+                            automatic language detection (default: None)
+      -b, --label-indices   Add token index labels to show the original token
+                            order; this can help in reading the trees, but it adds
+                            visual clutter (default: False)
+    
+
+The script takes input, makes a request to the Rosette API on your behalf, and converts the resulting JSON into an SVG image.  The SVG data is written to `/dev/stdout` by default, or to the given file path specified with the `-o/--output` option.
+
+Note: If you want to circumvent supplying your API key with `-k/--key` (or typing it at the prompt) on every execution of the script, you can set a `ROSETTE_USER_KEY` enviroment variable:
+
+    $ echo "export ROSETTE_USER_KEY=<your-user-key>" >> ~/.bash_profile
+    $ . ~/.bash_profile
+
+###Examples
+`deps_to_graph.py` can read input from `/dev/stdin`, a file path, or a URI.
+
+####Reading from `stdin`
+
+    $ echo "Let's make a graph" | ./deps_to_graph.py > graph.svg
+    
+You can look at the resulting `graph.svg` by opening it in your web browser.
+
+It will look like this:
+
+![graph.svg](graph.svg)
+
+###Reading from a file
+
+	# create a sample text file to read from
+	$ echo "The rules governing the hierarchical structure of a sentence are called “syntax” in linguistics. Knowing the structure of a sentence helps to understand how the words in a sentence are related to each other." > data.txt
+	# create an SVG visualization using the content of the text file as input
+	$ ./deps_to_graph.py -i data.txt > data.svg
+
+The results will look like this:
+
+![data.svg](data.svg)
+
+###Reading from a URI
+
+Another option is to rely on Rosette API to extract the textual content of a web page and parse it.  You can do this by giving a URI with the `-i/--input` option and additionally specifying the `-u/--content-uri` option to indicate that the input is a URI:
+
+	$ ./deps_to_graphy.py -i 'www.your-favorite-site.com' -u > your-favorite-site.svg
+	
+####Token index labels
+One additional convenience option is the `-b/--label-indices` option.  This labels each node in the parse tree with an index.  This can help with reading the content from the graph as it shows the original order of the word tokens.  This is left off by default as it adds visual clutter, but it can be a useful option:
+
+	$ ./deps_to_graph.py -b -i data.txt > labeled-data.svg
+
+With token index labels, the trees looks like this:
+
+![labeled-data.svg](labeled-data.svg)
---
+++ deps_to_graph.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3
+"""Render Rosette API dependency parse trees as SVG via Graphviz"""
+
+import argparse
+import io
+import json
+import os
+import re
+import subprocess
+import sys
+import urllib
+
+from operator import itemgetter, methodcaller
+from getpass import getpass
+
+
+EXTERNALS = ('rosette_api',)
+try:
+    from rosette.api import API, DocumentParameters
+except ImportError:
+    message = '''This script depends on the following modules:
+    {}
+If you are missing any of these modules, install them with pip3:
+    $ pip3 install {}'''
+    print(
+        message.format('\n\t'.join(EXTERNALS), ' '.join(EXTERNALS)),
+        file=sys.stderr
+    )
+    sys.exit(1)
+
+DEFAULT_ROSETTE_API_URL = 'https://api.rosette.com/rest/v1/'
+
+ROOT = 'root'
+NODE = '{index} [label="{token}"]\n'
+
+EDGE = '{governorTokenIndex} -> {dependencyTokenIndex} [label="{relationship}"]'
+
+STYLE = '''
+edge [dir="forward", arrowhead="open", arrowsize=0.5]
+node [shape="box", height=0]
+'''
+
+def request(content, endpoint, api, language=None, uri=False, **kwargs):
+    """Request Rosette API results for the given content and endpoint.
+
+    This method gets the requested results from the Rosette API as JSON.  If
+    api's output parameter has been set to "rosette" then the JSON will consist
+    of an A(nnotated) D(ata) M(odel) or ADM.  An ADM is a Python dict
+    representing document content, annotations of the document content,
+    and document metadata.
+    
+    content:  path or URI of a document for the Rosette API to process
+    endpoint: a Rosette API endpoint string (e.g., 'entities')
+              (see https://developer.rosette.com/features-and-functions)
+    api:      a rosette.api.API instance
+              (e.g., API(user_key=<key>, service_url=<url>))
+    language: an optional ISO 639-2 T language code
+              (the Rosette API will automatically detect the language of the
+              content by default)
+    uri:      specify that the content is to be treated as a URI and the
+              the document content is to be extracted from the URI
+    kwargs:   additional keyword arguments
+              (e.g., if endpoint is 'morphology' you can specify facet='lemmas';
+              see https://developer.rosette.com/features-and-functions for
+              complete documentation)
+    
+    For example:
+    
+    api = API(user_key=<key>, service_url=DEFAULT_ROSETTE_API_URL)
+    
+    response = request('This is a sentence.', 'syntax_dependencies', api)
+    response['sentences'] -> [
+        {
+            "startTokenIndex": 0,
+            "endTokenIndex": 4,
+            "dependencies": [
+                {
+                    "dependencyType": "nsubj",
+                    "governorTokenIndex": 3,
+                    "dependentTokenIndex": 0
+                },
+                {
+                    "dependencyType": "cop",
+                    "governorTokenIndex": 3,
+                    "dependentTokenIndex": 1
+                },
+                {
+                    "dependencyType": "det",
+                    "governorTokenIndex": 3,
+                    "dependentTokenIndex": 2
+                },
+                {
+                    "dependencyType": "root",
+                    "governorTokenIndex": -1,
+                    "dependentTokenIndex": 3
+                },
+                {
+                    "dependencyType": "punct",
+                    "governorTokenIndex": 3,
+                    "dependentTokenIndex": 4
+                }
+            ]
+        }
+    ]
+    response['tokens'] -> ['This', 'is', 'a', 'sentence', '.']
+    
+    api.setUrlParameter('output', 'rosette')
+    
+    response = request('This is a sentence.', 'syntax_dependencies', api)
+    response['attributes']['dependency']['items'] -> [
+        {
+            "relationship": "nsubj",
+            "governorTokenIndex": 3,
+            "dependencyTokenIndex": 0
+        },
+        {
+            "relationship": "cop",
+            "governorTokenIndex": 3,
+            "dependencyTokenIndex": 1
+        },
+        {
+            "relationship": "det",
+            "governorTokenIndex": 3,
+            "dependencyTokenIndex": 2
+        },
+        {
+            "relationship": "root",
+            "governorTokenIndex": -1,
+            "dependencyTokenIndex": 3
+        },
+        {
+            "relationship": "punct",
+            "governorTokenIndex": 3,
+            "dependencyTokenIndex": 4
+        }
+    ]
+    
+    response['attributes']['token']['items'] -> [
+        {
+            "startOffset": 0,
+            "endOffset": 4,
+            "text": "This"
+        },
+        {
+            "startOffset": 5,
+            "endOffset": 7,
+            "text": "is"
+        },
+        {
+            "startOffset": 8,
+            "endOffset": 9,
+            "text": "a"
+        },
+        {
+            "startOffset": 10,
+            "endOffset": 18,
+            "text": "sentence"
+        },
+        {
+            "startOffset": 18,
+            "endOffset": 19,
+            "text": "."
+        }
+    ]
+
+    """
+    parameters = DocumentParameters()
+    if uri:
+        parameters['contentUri'] = content
+    else:
+        parameters['content'] = content
+    parameters['language'] = language
+    adm = methodcaller(endpoint, parameters, **kwargs)(api)
+    return adm
+
+def escape(token):
+    """Escape characters that have special semantics within GraphViz"""
+    pattern = re.compile(r'([\[\]()"\\])')
+    return pattern.sub(r'\\\1', token)
+
+def extent(obj):
+    """Get the start and end offset attributes of a dict-like object"""
+    return obj.get('startOffset', -1), obj.get('endOffset', -1)
+
+def tokens(adm):
+    """Get a sorted list of tokens from the ADM"""
+    return sorted(adm['attributes']['token']['items'], key=extent)
+
+def dependencies(adm):
+    """Get a sorted list of dependency edges from the ADM"""
+    return sorted(
+        adm['attributes']['dependency']['items'],
+        key=itemgetter('governorTokenIndex', 'dependencyTokenIndex')
+    )
+
+def deps_to_graph(adm, index_labels=False):
+    """Create a digraph whose nodes are tokens and edges are dependencies"""
+    sentence_index = -1
+    digraph = 'digraph G{{{}'.format(STYLE)
+    for i, token in enumerate(tokens(adm)):
+        index_label = '({}) '.format(i) if index_labels else ''
+        token_text = '{}{}'.format(index_label, escape(token['text']))
+        digraph += NODE.format(index=i, token=token_text)
+    for edge in dependencies(adm):
+        if edge['relationship'] == ROOT:
+            digraph += '{} [label="S{}"]'.format(sentence_index, -sentence_index)
+            edge['governorTokenIndex'] = sentence_index
+            sentence_index -= 1
+        digraph += EDGE.format(**edge) + '\n'
+    digraph += '}\n'
+    return digraph
+
+def make_svg(digraph):
+    """Get an SVG from a digraph string (relies on GraphViz)"""
+    try:
+        process = subprocess.Popen(
+            ['dot', '-Tsvg'],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+    except OSError:
+        message = '''Cannot find dot which is required to create the SVG.
+(You can install dot from the Graphviz package: http://graphviz.org/)'''
+        raise Exception(message)
+    svg, stderr = process.communicate(digraph)
+    if stderr:
+        print(stderr, file=sys.stderr)
+        message = 'Failed to create an svg representation from string: {}'
+        raise Exception(message.format(digraph))
+    return svg
+
+def get_content(content, uri=False):
+    """Load content from file or stdin"""
+    if content is None:
+        content = sys.stdin.read()
+    elif os.path.isfile(content):
+        with open(content, mode='r') as f:
+            content = f.read()
+    # Rosette API may balk at non-Latin characters in a URI so we can get urllib
+    # to %-escape the URI for us
+    if uri:
+        unquoted = urllib.parse.unquote(content)
+        content = urllib.parse.quote(unquoted, '/:')
+    return content
+
+def dump(data, filename):
+    if filename is None:
+        print(data, file=sys.stdout)
+    else:
+        with open(filename, mode='w') as f:
+            print(data, file=f)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=__doc__
+    )
+    parser.add_argument(
+        '-i', '--input',
+        help=(
+            'Path to a file containing input data (if not specified data is '
+            'read from stdin)'
+        ),
+        default=None
+    )
+    parser.add_argument(
+        '-u',
+        '--content-uri',
+        action='store_true',
+        help='Specify that the input is a URI (otherwise load text from file)'
+    )
+    parser.add_argument(
+        '-o', '--output',
+        help=(
+            'Path to a file where SVG will be written (if not specified data '
+            'is written to stdout)'
+        ),
+        default=None
+    )
+    parser.add_argument(
+        '-k', '--key',
+        help='Rosette API Key',
+        default=None
+    )
+    parser.add_argument(
+        '-a', '--api-url',
+        help='Alternative Rosette API URL',
+        default=DEFAULT_ROSETTE_API_URL
+    )
+    parser.add_argument(
+        '-l', '--language',
+        help=(
+            'A three-letter (ISO 639-2 T) code that will override automatic '
+            'language detection'
+        ),
+        default=None
+    )
+    parser.add_argument(
+        '-b', '--label-indices',
+        action='store_true',
+        help=(
+            'Add token index labels to show the original token order; '
+            'this can help in reading the trees, but it adds visual clutter'
+        )
+    )
+    args = parser.parse_args()
+    # Get the user's Rosette API key
+    key = (
+        os.environ.get('ROSETTE_USER_KEY') or
+        args.key or
+        getpass(prompt='Enter your Rosette API key: ')
+    )
+    # Instantiate the Rosette API
+    api = API(user_key=key, service_url=args.api_url)
+    api.setUrlParameter('output', 'rosette')
+    content = get_content(args.input, args.content_uri)
+    adm = request(
+        content,
+        'syntax_dependencies',
+        api,
+        language=args.language,
+        uri=args.content_uri
+    )
+    dump(make_svg(deps_to_graph(adm, args.label_indices)), args.output)
